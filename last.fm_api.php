2f9fc6186d27f6ff4fc641b989cb1e6ddec08f29
IgordosDev <igordos@vivaldi.net>
2026-04-13 22:55:34 +0300

    спасибо ласт фм за код 500

--- last.fm_api.php
+++ last.fm_api.php
@@ -2,7 +2,7 @@
     $curlSession = curl_init();
     $api_user = $_ENV["lfm.user"];
     $api_key = $_ENV["lfm.key"];
-    curl_setopt($curlSession, CURLOPT_URL, "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$api_user&api_key=$api_key&format=json&limit=1");
+    curl_setopt($curlSession, CURLOPT_URL, "https://api.listenbrainz.org/1/lastfm/2.0/?method=user.getrecenttracks&user=$api_user&api_key=$api_key&format=json&limit=1");
     curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
     curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
     
